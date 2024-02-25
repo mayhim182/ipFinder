902e63775aaec9bb11287f4ab4657c779ac1e340
mayhim182 <mayankatnitrkl@gmail.com>
2024-02-25 18:42:25 +0530

    daily code push

--- src/App.js
+++ src/App.js
@@ -2,23 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  //Setting up initial state variables
+  const [ipDetails, setIpDetails] = useState([]);
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
+
+  //Fetching the API once when the 
+  //Component is mounted
+
+  useEffect(()=>{
+    Axios.get('https://ipapi.co/json/').then((res)=>{
+      setIpDetails(res.data);
+      setLat(res.data.latitude);
+      setLon(res.data.longitude);
+    });
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="heading">IP Address Finder</h1>
+      <div className="App">
+        <div className="left">
+          <h4>What is the IPv4 address?</h4>
+          <h1 id="ip">{ipDetails.ip}</h1>
+          <h4>Approximate location: </h4>
+          <p>{ipDetails.city}, {ipDetails.region},
+          {ipDetails.country_name}.</p>
+
+          <h4>Internet Service Provider(ISP):</h4>
+          <p>{ipDetails.org}</p>
+        </div>
+        <Map lat={lat} lon={lon} />
+      </div>
+    </>
   );
 }
 
